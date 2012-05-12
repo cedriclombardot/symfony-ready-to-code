e5c4e1ecb946cb4accd55f008b42175cc39f25f7
Cedric LOMBARDOT <cedric.lombardot@gmail.com>
2012-05-12 11:15:00 +0200

    Enable to build backend bundle propel schemas

--- rebuild.sh
+++ rebuild.sh
@@ -11,9 +11,11 @@ php frontend/console propel:database:create --connection=default
 
 # Rebuild propel models
 php frontend/console propel:build --verbose
+php backend/console propel:build --verbose
 
 # Insert propel sql
 php frontend/console propel:sql:insert --force --connection=default
+php backend/console propel:sql:insert --force --connection=default
 # php frontend/console propel:fixtures:load --dir=src/You/YourBundle/Resources/Fixtures/ --connection=default
 
 # Create the demo user fos
