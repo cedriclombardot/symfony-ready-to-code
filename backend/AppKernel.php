fb0311427cd8942643c4709ea942ff6517382070
clombardot <cedric.lombardot@gmail.com>
2012-05-29 15:33:14 +0200

    Setup vbardales/MultipleAppKernelBundle

--- backend/AppKernel.php
+++ backend/AppKernel.php
@@ -1,32 +1,20 @@
 <?php
 
+require_once __DIR__.'/../commons/BaseKernel.php';
+
 // For propel / doctrine uploads : getUploadRootDir() => __DIR__.'/../../../../web/', KERNEL_DIR.'/../web/'
 define('KERNEL_DIR', __DIR__);
 
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-class AppKernel extends Kernel
+class AppKernel extends BaseKernel
 {
-    public function registerBundles()
+    public function registerAppBundles()
     {
         $bundles = array(
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-
             // Forms Extra
             new Trsteel\CkeditorBundle\TrsteelCkeditorBundle(),
-
-            // Propel
-            new Propel\PropelBundle\PropelBundle(),
 
             // Admingen
             new Admingenerator\GeneratorBundle\AdmingeneratorGeneratorBundle(),
@@ -37,17 +25,6 @@ class AppKernel extends Kernel
             new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
 
         );
-
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-        }
-
-        if ('test' === $this->getEnvironment()) {
-            $bundles[] = new Behat\BehatBundle\BehatBundle();
-            $bundles[] = new Behat\MinkBundle\MinkBundle();
-        }
 
         return $bundles;
     }
---
+++ commons/BaseKernel.php
@@ -0,0 +1,41 @@
+<?php
+
+use MultipleApp\KernelBundle\Kernel\Kernel;
+
+abstract class BaseKernel extends Kernel
+{
+    public function registerCommonsBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\AopBundle\JMSAopBundle(),
+            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
+            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+
+            // Propel
+            new Propel\PropelBundle\PropelBundle(),
+
+            // Multiple App
+            new MultipleApp\KernelBundle\MultipleAppKernelBundle(),
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        }
+
+        if ('test' === $this->getEnvironment()) {
+            $bundles[] = new Behat\BehatBundle\BehatBundle();
+            $bundles[] = new Behat\MinkBundle\MinkBundle();
+        }
+
+        return $bundles;
+    }
+}
