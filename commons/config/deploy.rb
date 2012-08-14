14a190335de6964a7da96cb0a6b60d48ad7419ed
cedric lombardot <cedric.lombardot@gmail.com>
2012-08-14 22:06:09 +0300

    Add speed deploy for capistrano config
    http://capifony.org/cookbook/speeding-up-deploy.html

--- commons/config/deploy.rb
+++ commons/config/deploy.rb
@@ -30,3 +30,17 @@ set :use_composer, true
 
 set :apps, %w(frontend backend)
 
+before 'symfony:composer:install', 'composer:copy_vendors'
+before 'symfony:composer:update', 'composer:copy_vendors'
+
+namespace :composer do
+  task :copy_vendors, :except => { :no_release => true } do
+    pretty_print "--> Copy vendor file from previous release"
+
+    run "vendorDir=#{current_path}/vendor; if [ -d $vendorDir ] || [ -h $vendorDir ]; then cp -a $vendorDir #{latest_release}/vendor; fi;"
+    puts_ok
+  end
+end
+
+
+
